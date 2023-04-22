1577df1df544f01fefc7a5db9b2f8f374b60107e
NevanOne <gaalfonzo@gmail.com>
2023-04-22 15:24:08 -0300

    Actualizado Stock

--- src/data/stock.js
+++ src/data/stock.js
@@ -2,44 +2,44 @@ export const stock = [
 
     {
         id: 1,
-        description: "Pantalón",
-        price: 35,
-        image: "https://picsum.photos/200/300",
-        categoria: "ropa"
+        description: "The Witcher",
+        price: 4500,
+        image: "https://media.vandal.net/m/3557/201186111756_1.jpg",
+        categoria: "Juegos"
     },
     {
         id: 2,
-        description: "Televisor",
-        price: 352,
-        image: "https://picsum.photos/id/237/200/300",
-        categoria: "electrodomestico"
+        description: "Grand Theft Auto 4",
+        price: 5000,
+        image: "https://m.media-amazon.com/images/I/51uZOV8M61L.jpg",
+        categoria: "Juegos"
     },
     {
         id: 3,
-        description: "Sueter",
-        price: 200,
-        image: "https://picsum.photos/200/300?grayscale",
-        categoria: "ropa"        
+        description: "Mafia 2",
+        price: 2300,
+        image: "https://m.media-amazon.com/images/I/81XREvo5PIS._SY445_.jpg",
+        categoria: "Juegos"        
     },
     {
         id: 4,
-        description: "Remera",
-        price: 35,
-        image: "https://picsum.photos/200/300",
-        categoria: "ropa"
+        description: "Crysis Wathead",
+        price: 2000,
+        image: "hhttps://c8.alamy.com/compes/2h7agt7/video-juego-shooter-para-windows-pc-dvd-cubierta-de-2000s-crysis-warhead-desarrollado-por-crytek-lanzado-en-2008-2h7agt7.jpg",
+        categoria: "Juegos"
     },
     {
         id: 5,
-        description: "Celular",
-        price: 345,
-        image: "https://picsum.photos/id/237/200/300",
-        categoria: "electrodomestico"
+        description: "God of War 2",
+        price: 3255,
+        image: "https://rukminim1.flixcart.com/image/416/416/jpa2w7k0/physical-game/g/r/h/standard-edition-god-of-war-2-pc-dvd-full-game-windows-original-imafbgty9tytzm9j.jpeg?q=70",
+        categoria: "Juegos"
     },
     {
         id: 6,
-        description: "Microondas",
-        price: 2945,
-        image: "https://picsum.photos/200/300?grayscale",
-        categoria: "electrodomestico"
+        description: "Minecraft Global Key",
+        price: 4000,
+        image: "https://http2.mlstatic.com/D_NQ_NP_633037-MLA29337798530_022019-O.webp",
+        categoria: "Juegos"
     },
 ]
